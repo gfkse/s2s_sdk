c9770ca99ad3ebc2d22aa8659ae92530a9c08984
Christian Baron <christianbaron@MacBook-Pro-von-Christian.local>
2022-08-04 11:47:50 +0200

    Update

--- Package.swift
+++ Package.swift
@@ -4,13 +4,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "S2S_SDK",
+    name: "S2S_GoogleIMA",
     platforms: [.iOS(.v9)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "S2S_SDK",
-            targets: ["s2s_sdk_ios"])
+            name: "S2S_GoogleIMA",
+            targets: ["S2S_GoogleIMA"])
     ],
     dependencies: [
     ],
@@ -18,9 +18,9 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on
         .binaryTarget(
-             name: "s2s_sdk_ios",
+             name: "S2S_GoogleIMA",
              url:
-               "https://s3.eu-central-1.amazonaws.com/download.sensic.net/s2s/sdk/ios/testing/1/s2s_sdk_ios_2.zip",
+               "https://s3.eu-central-1.amazonaws.com/download.sensic.net/s2s/sdk/ios/testing/1/S2S_GoogleIMA_1.zip",
              checksum: ""
            ),
 
