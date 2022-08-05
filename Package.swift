5ff1c3982f58b11592db635d494309ca30e40baa
Bamboo <bamboo@mbpsensooagent2.gfk.com>
2022-08-05 13:24:05 +0200

    Update

--- Package.swift
+++ Package.swift
@@ -10,7 +10,6 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "S2S_SDK",
-            type: .dynamic,
             targets: ["s2s_sdk_ios"])
     ],
     dependencies: [
@@ -22,7 +21,7 @@ let package = Package(
              name: "s2s_sdk_ios",
              url:
                "https://s3.eu-central-1.amazonaws.com/download.sensic.net/s2s/sdk/ios/testing/1/s2s_sdk_ios_1.zip",
-             checksum: "99148b3bceae12f2c6962c09df7a4c296941d5a3b3429c1438139f220f74f319"
+             checksum: "7adc094d6a6233366fd72d076aa2812f89b3a83cb9fa8da0aae79c0ba7b96e4d"
            ),
 
     ]
